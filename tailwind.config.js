7b8b1b019c8099aceb2dd3d510c7761cda2f333f
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-17 23:50:31 +0300

    add config files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    plugin(({ addComponents, theme, addUtilities }) => {
+      addUtilities({
+        ".flex-center-between": {
+          display: "flex",
+          alignitems: "center",
+          justifyContent: "space-between",
+        },
+      });
+    }),
+  ],
+};
