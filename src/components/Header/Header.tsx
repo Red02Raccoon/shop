6e840aa9a3536ba005f68cc372c91955499eff1f
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-22 23:36:03 +0300

    update Header with cart button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 
+import { Button, Badge } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
 import styles from "./Header.module.scss";
 
 interface Props {
   count: number;
+  onCartOpen: () => void;
 }
 
-const Header: React.FC<Props> = ({ count }) => {
+const Header: React.FC<Props> = ({ count, onCartOpen }) => {
   return (
     <div className={styles.header}>
       <div>Header</div>
 
-      <div>Count {count}</div>
+      <Badge count={count}>
+        <Button
+          className={styles.button}
+          type="primary"
+          icon={<ShoppingCartOutlined />}
+          size={"large"}
+          onClick={onCartOpen}
+        />
+      </Badge>
     </div>
   );
 };
