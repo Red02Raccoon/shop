b750e51b3a9ae1237889795f6dd26127365a2763
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 22:21:04 +0300

    fix styles for 404 page

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -10,6 +10,7 @@ const PageNotFound = () => {
 
     return (
         <Result
+            className="my-auto"
             status="404"
             title="404"
             subTitle="Sorry, the page you visited does not exist."
