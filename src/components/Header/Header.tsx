9a4d3c988d95a3cdc74156f8ac3080b3869c92c9
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 22:20:51 +0300

    add handle for logo click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import { NavBar } from '../NavBar';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
     return (
         <div className={styles.header}>
-            <div>Header</div>
+            <div onClick={handleLogoClick}>Header</div>
 
             <NavBar />
         </div>
