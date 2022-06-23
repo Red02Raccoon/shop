a29add19c6469b0c4bb2820953e9100d6ac4ed2a
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 21:37:41 +0300

    update app with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,14 @@
-import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { IProductItem } from 'common/types';
-import { ProductItem, Header, CartDrawer } from 'components';
+import { Header } from 'components';
 
 const App = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [products, setProducts] = useState<IProductItem[]>([]);
-    const [selectedProductsIds, setSelectedProductsIds] = useState<number[]>([]);
-
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then((res) => res.json())
-            .then((json) => {
-                console.log(json);
-                setProducts(json);
-            });
-    }, []);
-
-    const handleProductAdd = (id: number) => () => {
-        const newData = [...selectedProductsIds];
-        const index = selectedProductsIds.indexOf(id);
-
-        if (index >= 0) {
-            newData.splice(index, 1);
-        } else {
-            newData.splice(newData.length, 0, id);
-        }
-
-        setSelectedProductsIds(newData);
-    };
-
-    const handleDrawerToggle = () => {
-        setIsDrawerOpen((prev) => !prev);
-    };
-
     return (
         <div className="bg-white">
-            <Header count={selectedProductsIds.length} onCartOpen={handleDrawerToggle} />
+            <Header />
 
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-20 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="mb-10 text-2xl">Products</h2>
-
-                <div className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    {products.map(({ id, title, image, price, description }) => (
-                        <ProductItem
-                            key={id}
-                            id={id}
-                            title={title}
-                            image={image}
-                            price={price}
-                            description={description}
-                            isAdded={!!selectedProductsIds.includes(id)}
-                            onProductAdd={handleProductAdd(id)}
-                        />
-                    ))}
-                </div>
-
-                <CartDrawer
-                    isOpen={isDrawerOpen}
-                    products={products.filter(({ id }) => selectedProductsIds.includes(id))}
-                    onClose={handleDrawerToggle}
-                />
+                <Outlet />
             </div>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import 'antd/dist/antd.css';
 import './index.scss';
+
+import { Shop } from './pages';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="/" element={<Shop />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
