e6fd4bff6df1ea57d44dc435aac39862f3e118f3
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 22:15:58 +0300

    add nested routes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
+import { NavBar } from '../NavBar';
+
 import styles from './Header.module.scss';
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <div>Header</div>
+
+            <NavBar />
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './index.scss';
 
-import { Shop } from './pages';
+import { PageNotFound, Shop } from 'pages';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -15,8 +16,18 @@ root.render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />}>
-                    <Route path="/" element={<Shop />} />
+                    <Route
+                        index
+                        element={
+                            <main style={{ padding: '1rem' }}>
+                                <p>Select a page</p>
+                                <p>I am checking how index prop works in react-router</p>
+                            </main>
+                        }
+                    />
+                    <Route path="/shop" element={<Shop />} />
                 </Route>
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
