756d0b43421f1346531545a8a3c5948e6111c903
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 21:37:22 +0300

    create shop page

---
+++ src/pages/Shop/Shop.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+
+import { ProductItem, CartDrawer, CartButton } from 'components';
+import { IProductItem } from 'common/types';
+
+const Shop = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [products, setProducts] = useState<IProductItem[]>([]);
+    const [selectedProductsIds, setSelectedProductsIds] = useState<number[]>([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json);
+                setProducts(json);
+            });
+    }, []);
+
+    const handleProductAdd = (id: number) => () => {
+        const newData = [...selectedProductsIds];
+        const index = selectedProductsIds.indexOf(id);
+
+        if (index >= 0) {
+            newData.splice(index, 1);
+        } else {
+            newData.splice(newData.length, 0, id);
+        }
+
+        setSelectedProductsIds(newData);
+    };
+
+    const handleDrawerToggle = () => {
+        setIsDrawerOpen((prev) => !prev);
+    };
+
+    return (
+        <div>
+            <div className="flex flex-row">
+                <h2 className="mb-10 text-2xl mr-auto">Products</h2>
+
+                <CartButton count={selectedProductsIds.length} onCartOpen={handleDrawerToggle} />
+            </div>
+
+            <div className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {products.map(({ id, title, image, price, description }) => (
+                    <ProductItem
+                        key={id}
+                        id={id}
+                        title={title}
+                        image={image}
+                        price={price}
+                        description={description}
+                        isAdded={!!selectedProductsIds.includes(id)}
+                        onProductAdd={handleProductAdd(id)}
+                    />
+                ))}
+            </div>
+
+            <CartDrawer
+                isOpen={isDrawerOpen}
+                products={products.filter(({ id }) => selectedProductsIds.includes(id))}
+                onClose={handleDrawerToggle}
+            />
+        </div>
+    );
+};
+
+export default Shop;
