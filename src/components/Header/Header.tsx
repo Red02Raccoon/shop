6acbe98a73a59a597ccbe9b7c4d8939b39d4bc6e
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-23 21:36:31 +0300

    remove cart button from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,9 @@
-import React from 'react';
-
-import { Button, Badge } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons';
-
 import styles from './Header.module.scss';
 
-interface Props {
-    count: number;
-    onCartOpen: () => void;
-}
-
-const Header: React.FC<Props> = ({ count, onCartOpen }) => {
+const Header = () => {
     return (
         <div className={styles.header}>
             <div>Header</div>
-
-            <Badge count={count}>
-                <Button
-                    className={styles.button}
-                    type="primary"
-                    icon={<ShoppingCartOutlined />}
-                    size={'large'}
-                    onClick={onCartOpen}
-                />
-            </Badge>
         </div>
     );
 };
