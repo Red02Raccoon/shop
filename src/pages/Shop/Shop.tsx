d7563b4a7a4bc3132762f0566457a3ae9fa83663
Anastasiya Chervona <red02raccoon@gmail.com>
2022-06-29 15:48:27 +0300

    add loading state to progucts

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
+import { Spin } from 'antd';
+
 import { ProductItem, CartDrawer, CartButton } from 'components';
 import { IProductItem } from 'common/types';
 
 const Shop = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [products, setProducts] = useState<IProductItem[]>([]);
+
     const [selectedProductsIds, setSelectedProductsIds] = useState<number[]>([]);
 
     useEffect(() => {
+        setIsLoading(true);
         fetch('https://fakestoreapi.com/products')
             .then((res) => res.json())
             .then((json) => {
                 console.log(json);
                 setProducts(json);
+            })
+            .finally(() => {
+                setIsLoading(false);
             });
     }, []);
 
@@ -42,20 +51,22 @@ const Shop = () => {
                 <CartButton count={selectedProductsIds.length} onCartOpen={handleDrawerToggle} />
             </div>
 
-            <div className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                {products.map(({ id, title, image, price, description }) => (
-                    <ProductItem
-                        key={id}
-                        id={id}
-                        title={title}
-                        image={image}
-                        price={price}
-                        description={description}
-                        isAdded={!!selectedProductsIds.includes(id)}
-                        onProductAdd={handleProductAdd(id)}
-                    />
-                ))}
-            </div>
+            <Spin spinning={isLoading}>
+                <div className="grid grid-cols-1 gap-y-20 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 min-h-screen">
+                    {products.map(({ id, title, image, price, description }) => (
+                        <ProductItem
+                            key={id}
+                            id={id}
+                            title={title}
+                            image={image}
+                            price={price}
+                            description={description}
+                            isAdded={!!selectedProductsIds.includes(id)}
+                            onProductAdd={handleProductAdd(id)}
+                        />
+                    ))}
+                </div>
+            </Spin>
 
             <CartDrawer
                 isOpen={isDrawerOpen}
